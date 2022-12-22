9329a26a972556389109c3f5209d1b76706fdf64
lepa15 <lepa2604@gmail.com>
2022-12-22 20:03:31 +0200

    add config package

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+module.exports = {
+	mode: 'development',
+   entry: './src/js/script.js',
+   output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'public')
+   },
+   module: {
+      rules: [{
+         test:/\.(s*)css$/,
+         use: [
+            miniCss.loader,
+            'css-loader',
+            'sass-loader',
+         ]
+      }]
+   },
+   plugins: [
+      new miniCss({
+         filename: 'style.css',
+      }),
+   ]
+};
