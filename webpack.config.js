e66d22aa71ae7aee05ebfba7b196ab5b981eb2c4
lepa15 <lepa2604@gmail.com>
2022-12-24 12:03:42 +0200

    add task

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
    entry: './src/js/script.js',
    output: {
       filename: 'index.js',
-      path: path.resolve(__dirname, 'public')
+      path: path.resolve(__dirname, 'public/js')
    },
    module: {
       rules: [{
